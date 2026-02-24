91fdee0d3e382509c4a09bd51b5fcae61d2cd081
Disheng WU <ecore.cn@gmail.com>
2026-02-24 23:59:27 +0800

    Rename.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,7 +69,7 @@ android {
         debug {
             applicationIdSuffix = ".debug"
             resValue("string", "app_launcher_name", "Image Toolbox R3 DEBUG")
-            resValue("string", "file_provider", "com.t8rin.imagetoolbox.fileprovider.debug")
+            resValue("string", "file_provider", "com.t8rin.imagetoolbox.fileprovider.rename03.debug")
         }
         release {
             isMinifyEnabled = true
@@ -79,7 +79,7 @@ android {
                 "proguard-rules.pro"
             )
             resValue("string", "app_launcher_name", "Image Toolbox R3")
-            resValue("string", "file_provider", "com.t8rin.imagetoolbox.fileprovider")
+            resValue("string", "file_provider", "com.t8rin.imagetoolbox.fileprovider.rename03")
         }
         create("benchmark") {
             initWith(buildTypes.getByName("release"))
